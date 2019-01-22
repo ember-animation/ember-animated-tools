8e3285da0a5ba1048b305303eabd7538fc6dca39
Edward Faulkner <ef@alum.mit.edu>
2019-01-22 17:05:08 -0500

    adding keyboard hide/show support

--- addon/components/animated-tools.js
+++ addon/components/animated-tools.js
@@ -4,12 +4,68 @@ import { computed } from '@ember/object';
 
 export default Component.extend({
   layout,
-  classNames: 'animated-tools',
-  classNameBindings: ['isOpen'],
+  tagName: '',
+
+  tests: computed(function() {
+    if (!this.get('hideUntilKeys')) {
+      return null;
+    }
+    return this.get('hideUntilKeys').split('-').map(part => {
+      if (part === 'Ctrl') {
+        return (event) => event.ctrlKey;
+      }
+      if (part === 'Alt') {
+        return (event) => event.altKey;
+      }
+      if (part === 'Shift') {
+        return (event) => event.shiftKey;
+      }
+      return (event) => event.code === part;
+    });
+  }),
+
+  didInsertElement() {
+    if (this.get('hideUntilKeys')) {
+      this._keyListener = this._keyListener.bind(this);
+      document.addEventListener('keydown', this._keyListener);
+    }
+  },
+
+  willDestroyElement() {
+    if (this.get('hideUntilKeys')) {
+      document.removeEventListener('keydown', this._keyListener);
+    }
+  },
+
+  _keyListener(event) {
+    if (this.get('tests').every(test => test(event))) {
+      if (localStorage.getItem('animated-tools-activated')) {
+        localStorage.removeItem('animated-tools-activated');
+      } else {
+        localStorage.setItem('animated-tools-activated', true);
+        localStorage.setItem('animated-tools-open', true);
+      }
+      this.notifyPropertyChange('isHidden');
+      this.notifyPropertyChange('isOpen');
+    }
+  },
+
   isOpen: computed(function() {
     return typeof FastBoot === 'undefined' &&
       !!localStorage.getItem('animated-tools-open');
   }),
+
+  isHidden: computed(function() {
+    // always hidden in fastboot
+    return typeof FastBoot !== 'undefined' ||
+
+      // always hidden if we're not using the hideUntilKeys feature
+      !this.get('hideUntilKeys') ||
+
+      // or hidden if we haven't been acticvated by the keys yet
+      !localStorage.getItem('animated-tools-activated');
+  }),
+
   actions: {
     toggle() {
       if (this.get('isOpen')) {
